8b60d8a66ec58bdd07bdcaa3c9f99fdfd0537fb9
Michał <mmichalf@gmail.com>
2013-03-22 12:53:00 +0100

    działa o_O

--- src/Graph.java
+++ src/Graph.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  *
@@ -10,14 +11,19 @@ public class Graph
 {
     private List<Integer> next;
     private List<Integer> head;
-    
+    private List<Boolean> discovered;
+        
     public Graph()
     {
         next = new ArrayList<>();
         head = new ArrayList<>();
+        discovered = new ArrayList<>();
         
         for(int i=0;i<Main.GRAPH_SIZE;++i)
+        {
             next.add(-1);
+            discovered.add(Boolean.FALSE);
+        }
         
     }
     
@@ -25,7 +31,7 @@ public class Graph
     {
         if(next.get(a) < 0)
         {
-            next.set(a, head.size()+Main.GRAPH_SIZE+1);
+            next.set(a, head.size()+Main.GRAPH_SIZE);
             
             head.add(b);
             
@@ -60,24 +66,53 @@ public class Graph
         return head;
     }
     
-    public void PrintList(List<Integer> listToPrint)
+    public List<Boolean> GetDiscoveredList()
     {
-        if(listToPrint!=null)
+        return discovered;
+    }
+    public void PrintList()
+    {
+        for(int i=0;i<Main.GRAPH_SIZE;++i)
         {
-            for(Integer v : listToPrint)
+            System.out.print("["+i+"]:");
+            int begin = next.get(i);
+            while( begin > -1)
             {
-                System.out.println(v);
+                System.out.print(" -> ["+begin + "]: "+head.get(begin-Main.GRAPH_SIZE));
+                begin = next.get(begin);
+            }
+            System.out.println("");
+        }
+    }
+    
+    public void DFS(int v_index)
+    {
+        Stack stack = new Stack();
+        stack.push(v_index);
+        while(stack.empty() != true)
+        {
+            int t = (int) stack.pop();
+
+            //System.out.print(t+" ");
+            discovered.set(t, Boolean.TRUE);
+            
+            int begin = next.get(t);
+            while( begin > -1)
+            {
+                if(!discovered.get(head.get(begin-Main.GRAPH_SIZE)))
+                    stack.push(head.get(begin-Main.GRAPH_SIZE));
+                begin = next.get(begin);
             }
         }
     }
 }
 
-class Vertex
-{
-    private int id;
-}
-
-class Edge
-{
-    
-}
+//class Vertex
+//{
+//    private int id;
+//}
+//
+//class Edge
+//{
+//    
+//}
--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,19 @@ import java.util.Scanner;
  */
 public class Main 
 {
-    public static Integer GRAPH_SIZE;
+    public static int GRAPH_SIZE;
     public static void main(String[] args) 
     {
-        Graph g = new Graph();
-        
         try
         {
-            Main.GRAPH_SIZE = Integer.parseInt(args[1]);
+            Main.GRAPH_SIZE = Integer.parseInt(args[1])+1;
         }
         catch(NumberFormatException e)
         {
             System.err.println("BŁĄD ZŁY ROZMIAR"+e.getMessage());
         }
+        
+        Graph g = new Graph();
         
         File file = new File(args[0]);
         try 
@@ -33,6 +33,7 @@ public class Main
                 Integer b = sc.nextInt();
                 
                 g.addEdge(a, b);
+                g.addEdge(b, a);
             }
         } 
         catch (FileNotFoundException ex) 
@@ -40,8 +41,22 @@ public class Main
             System.err.println("BŁAD Z PARSOWANIEM PLIKU WEJSCIOWEGO"+ex.getMessage());
         }
         
-        g.PrintList(g.getNextList());
-        System.out.println(" - - ");
-        g.PrintList(g.getHeadList());
+        System.out.println("Ilość Vertex'ów: "+(Main.GRAPH_SIZE-1));
+        //g.PrintList();
+        
+        
+        int ilosc_skladowych_spojnych = 0;
+        for(int i=1;i<Main.GRAPH_SIZE;++i)
+        {
+            if(!g.GetDiscoveredList().get(i))
+            {
+                g.DFS(i);
+                //System.out.println(".");
+                ilosc_skladowych_spojnych++;
+            }
+        }
+        
+        System.out.println("Składowych spójnych : "+ilosc_skladowych_spojnych);
+        
     }    
 }
